1ca2694f50b97a05ac43b4eab2e4808958f1fc7f
akkinet <akashsharma.zak@gmail.com>
2024-11-12 17:50:39 +0530

    vendors list api

---
+++ app/api/vendors/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server"
+import { ddbDocClient } from "@/config/docClient";
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+export const GET = async req => {
+  try{
+    const params = {
+      TableName: "Vendors", 
+      // Limit: 10,                
+    };
+    const result = await ddbDocClient.send(new ScanCommand(params));
+    const vendors = result.Count > 0 ? result.Items : [];
+    return NextResponse.json(vendors, {status: 200})
+  }catch(error){
+    console.error(error);
+    return NextResponse.json({error: error.message}, {status: 500})
+  }
+}
