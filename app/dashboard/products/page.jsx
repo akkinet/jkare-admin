59fd472597a738442065dd3aa845c051772e1216
akkinet <akashsharma.zak@gmail.com>
2024-11-25 18:51:05 +0530

    build error free

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,13 +1,18 @@
 import ProductTable from "@/components/ProductTable";
+import { notFound } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
 const Product = async () => {
   const response = await fetch(`${process.env.API_URL}/product`);
+  if (response.status === 404) {
+    notFound();
+  }
   if (!response.ok) {
     throw new Error(`HTTP Error! Status: ${response.status}`);
   }
   const data = await response.json();
 
-  return <ProductTable data={data} />
+  return <ProductTable data={data} />;
 };
 
 export default Product;
